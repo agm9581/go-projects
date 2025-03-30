0e85fcf08d6528cbab57821eb0381d7337453fce
Antonio Gonzalez <agm9581@gmail.com>
2025-03-30 20:14:26 +0200

    Project: CRUD API finished

--- go-crud-api/README.md
+++ go-crud-api/README.md
@@ -8,3 +8,12 @@ Director ir a property of movie so we will build the struct with a pointer to th
 When creating the data for movies we have to indicate director as a pointer since the data is generated beforehand.
 
 Currently there's a get method that encodes the variable movieList based on the struct json structure provided and returns it, can be obtained via the getMoviesHandler function and the base route "/", each handler will provide access to each method. 
+
+The API now handles all requests:
+ - Get all movies with "/"
+ - Get one movie by the param provided after the base url "/{id}" or return not found
+ - Delete one movie by the param provided after the base url "/{id}" or return not found
+ - Create one movie on the base route "/", the request body json is decoded to verify type assertiveness with the Movies struct, if it's correct adds the new movie and returns all the movieList json encoded again.
+ - Update one movie uses the PUT method with "/{id}" url, the param will be extracted from the url, the movie will be found and deleted, then added afterwards with the same id as it originally had, if not found will inform about it.
+
+ In the future this api should be updated so it handles Generics to avoid redundant code.
--- go-crud-api/crud-api.go
+++ go-crud-api/crud-api.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"strconv"
 
@@ -24,8 +25,10 @@ type Director struct {
 
 var director1 = Director{Name: "Antonio", Surname: "Gonzalez", AverageRating: 10}
 var movieList []Movies = []Movies{{
-	Id: 1, Title: "Short film", Length: 2, Director: &director1,
-}, {Id: 2, Title: "Long film", Length: 1000, Director: &director1}}
+	Id: 1, Title: "Short film", Length: 2, Director: &director1},
+	{Id: 2, Title: "Long film", Length: 1000, Director: &director1},
+	{Id: 3, Title: "Long film", Length: 1000, Director: &director1},
+	{Id: 4, Title: "Long film", Length: 1000, Director: &director1}}
 
 func getMoviesHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -34,11 +37,75 @@ func getMoviesHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(movieList)
+
 }
+
+func getOneMovieHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	movieId := vars["id"]
+	for _, movie := range movieList {
+		if value, err := strconv.Atoi(movieId); err == nil && value == movie.Id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+
+	fmt.Fprintf(w, "Movie with id %s not found", movieId)
+
+}
+
+func deleteOneMovie(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	movieId := vars["id"]
+	for index, movie := range movieList {
+		if value, err := strconv.Atoi(movieId); err == nil && value == movie.Id {
+			w.Header().Set("Content-Type", "application/json")
+			movieList = append(movieList[:index], movieList[index+1:]...)
+			json.NewEncoder(w).Encode(movieList)
+			return
+		}
+	}
+
+	fmt.Fprintf(w, "Movie with id %s not found", movieId)
+
+}
+
+func createOneMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movies
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.Id = rand.Intn(100000)
+	movieList = append(movieList, movie)
+	json.NewEncoder(w).Encode(movieList)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	updateMovieId := vars["id"]
+	w.Header().Set("Content-Type", "application/json")
+	var updateMovie Movies
+	_ = json.NewDecoder(r.Body).Decode(&updateMovie)
+	for index, movie := range movieList {
+		if movieId, err := strconv.Atoi(updateMovieId); err == nil && movieId == movie.Id {
+			updateMovie.Id = movieId
+			movieList = append(movieList[:index], movieList[index+1:]...)
+			movieList = append(movieList, updateMovie)
+			json.NewEncoder(w).Encode(movieList)
+			return
+		}
+	}
+	fmt.Fprintf(w, "Movie with id %s not found", updateMovieId)
+}
+
 func main() {
 
 	router := mux.NewRouter()
-	router.HandleFunc("/", getMoviesHandler)
+	router.HandleFunc("/", getMoviesHandler).Methods("GET")
+	router.HandleFunc("/", createOneMovie).Methods("POST")
+	router.HandleFunc("/{id}", getOneMovieHandler).Methods("GET")
+	router.HandleFunc("/{id}", deleteOneMovie).Methods("DELETE")
+	router.HandleFunc("/{id}", updateMovie).Methods("PUT")
 
 	fmt.Println("Server started in port 8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
